a8878cccaba8436d86e4cd6e851a06394f9400e6
AshutoshDash1999 <dashashutosh1999@gmail.com>
2022-03-28 19:56:20 +0530

    add pi chart

--- src/App.js
+++ src/App.js
@@ -7,14 +7,15 @@ function App() {
   const [parsedData, setParsedData] = useState([]);
   const [tableRows, setTableRows] = useState([]);
   const [values, setValues] = useState([]);
+  const colors = ["#2A2135", "#6A2135", "#C13C37", "#E38627"];
 
   const changeHandler = (e) => {
     // console.log(e.target.files[0]);
     Papa.parse(e.target.files[0], {
       header: true,
       skipEmptyLines: true,
-      complete: function (results) {
-        console.log(results.data);
+      complete: (results) => {
+        // console.log(results.data);
         const rowsArray = [];
         const valuesArray = [];
 
@@ -29,16 +30,32 @@ function App() {
       },
     });
   };
-  
-  const valuesArray = []
-  const uniqueValueArray = []
-  if (values.length > 0){
-    for(let index=0; index<values.length; index++){
-      console.log(values[index][1])
-      valuesArray.push(values[index][1])
+
+  let valuesArray = [];
+  let uniqueValueArray = [];
+  let uniqueValueCount = {};
+  let pieData = [];
+  if (values.length > 0) {
+    for (let index = 0; index < values.length; index++) {
+      // console.log(values[index][1]);
+      valuesArray.push(values[index][1]);
     }
-    console.log(valuesArray);
+    // console.log(valuesArray);
+    uniqueValueArray = [...new Set(valuesArray)];
+    // console.log(uniqueValueArray);
+    
+    for (let index = 0; index < valuesArray.length; index++) {
+      uniqueValueCount[valuesArray[index]] = 1 + (uniqueValueCount[valuesArray[index]] || 0);
+    }
+
+    // let uniqueValueObject = Object.keys(uniqueValueCount).length;
+
+    Object.keys(uniqueValueCount).map((key) => {
+        console.log(uniqueValueCount[key]);
+    })
+
   }
+
 
 
   return (
@@ -73,13 +90,18 @@ function App() {
             })}
           </tbody>
         </table>
-        {/* <PieChart
-          data={[
-            { title: "One", value: 10, color: "#E38627" },
-            { title: "Two", value: 15, color: "#C13C37" },
-            { title: "Three", value: 20, color: "#6A2135" },
-          ]}
-        /> */}
+        <div style={{height:"100vh"}}>
+          {/* <PieChart
+            data={[
+              { title: "App", value: 2, color: "#E38627" },
+              { title: "ML", value: 3, color: "#C13C37" },
+              { title: "Web", value: 4, color: "#6A2135" },
+              { title: "iOS", value: 1, color: "#2A2135" },
+            ]}
+            label={({ dataEntry }) => dataEntry.title}
+            labelStyle={{fontSize: '5px', fontColor:"white"}}
+          /> */}
+        </div>
       </header>
     </div>
   );
